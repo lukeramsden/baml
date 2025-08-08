fdacd64aa9493feb3f5add67ef378566b0dcb8b4
Luke Ramsden <hello@lukeramsden.com>
2025-08-08 14:02:25 +0100

    fix: restore collectors variable usage in language_client_cffi functions.rs

--- engine/language_client_cffi/src/ffi/functions.rs
+++ engine/language_client_cffi/src/ffi/functions.rs
@@ -68,7 +68,7 @@ fn call_function_from_c_inner(
         kwargs,
         client_registry,
         env_vars,
-        collectors: _,
+        collectors,
         type_builder,
     } = BamlFunctionArguments::from_c_buffer(encoded_args, length)?;
 
@@ -138,7 +138,7 @@ fn call_function_parse_from_c_inner(
         kwargs,
         client_registry,
         env_vars,
-        collectors: _,
+        collectors,
         type_builder,
     } = BamlFunctionArguments::from_c_buffer(encoded_args, length)?;
 
@@ -246,7 +246,7 @@ fn call_function_stream_from_c_inner(
         kwargs,
         client_registry,
         env_vars,
-        collectors: _,
+        collectors,
         type_builder,
     } = BamlFunctionArguments::from_c_buffer(encoded_args, length)?;
 
